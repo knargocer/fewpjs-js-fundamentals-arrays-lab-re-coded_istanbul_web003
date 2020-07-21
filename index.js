70a627e099032140656e3e403d5b7e3f975d9097
Knar Gocer <knargr@gmail.com>
2020-07-21 09:15:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const removeLast =["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
-prepend.unshift("duck");
+prepend.unshift(" ");
 removeLast.pop();
 removeFirst.shift();
 prepend;
